1450b62169ccefa3f5a709d693a470b64042487f
miccoh1994 <36225227+miccoh1994@users.noreply.github.com>
2021-05-12 11:27:40 +0200

    Create dev.rollup.config.js

---
+++ dev.rollup.config.js
@@ -0,0 +1,14 @@
+import { terser } from 'rollup-plugin-terser';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import execute from 'rollup-plugin-execute';
+
+
+export default {
+	input: 'endpoint.js',
+	output: {
+		format: 'cjs',
+		file: 'index.js',
+	},
+	plugins: [nodeResolve(), commonjs(), terser(), execute(['docker-compose restart directus'])], //use any command here, e.g pm2 restart directus
+};
